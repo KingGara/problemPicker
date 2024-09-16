cdd09b65d19eaf0e9b38fbea809feb8ccf48b9ea
KingGara <evenoddvergara@gmail.com>
2024-09-16 11:07:08 -0700

    added stuff

--- problemPicker.js
+++ problemPicker.js
@@ -1,5 +1,5 @@
 /*
-    This is a leet code problem selector. As I continue to I'll update the 
+    This is a leet code problem selector. As I continue solving I'll update the 
     object. The idea is to randomly select a 'n' problems a day to complete 
     in preperation for a remote software engineering position.
 */
@@ -8,22 +8,27 @@ const problems = {
     mergeSortedArray: "Merge Sorted Array - Easy",
     removeElement: "Remove Element - Easy",
     removeDuplicatesFromSortedArray: "Remove Duplicates From Sorted Array - Easy",
-    removeDuplicatesFromSortedArrayII: "Remove Duplicated From Sorted Array II - Medium",
+    removeDuplicatesFromSortedArrayII: "Remove Duplicated From Sorted ArrayII - Medium",
     majorityElement: "Majority Element - Easy",
     rotateArray: "Rotate Array - Medium",
     bestTimeToBuyAndSellStock: "Best Time to Buy & Sell Stock - Easy",
     bestTimeToButAndSellStockII: "Best Time to But & Sell Stock - Medium",
     jumpGame: "Jump Game - Medium",
-    jumpGameII: "Jump Game II - Medium",
+    jumpGameII: "Jump GameII - Medium",
     hIndex: "h-Index - Medium",
     insertDeleteGetRandom: "Insert Delete Get Random - Medium"
 };
 
-const problemsObjectKeys = Object.keys(problems); // ALl keys within my object
+const problemsObjectKeys = Object.keys(problems); // All keys put into an array
 
-let randomIndex = Math.floor(Math.random() * problemsObjectKeys.length);
+const problemPicker = () => {
+    let randomIndex = Math.floor(Math.random() * problemsObjectKeys.length);
+    let randomKey = problemsObjectKeys[randomIndex]; // Select a random key within range 
+    let randomValue = problems[randomKey]; // Random value is selected
 
-let randomKey = problemsObjectKeys[randomIndex]; // Select a random key within range
-let randomValue = problems[randomKey];
+    return randomValue;
+};
 
-console.log(`${randomValue}`)
+
+
+console.log(problemPicker(problems));
